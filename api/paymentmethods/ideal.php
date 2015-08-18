2ca6882f908d5805f8c0cf112d5b7849eaf4f7be
Ricardo Jacobs <ricardozegt@gmail.com>
2015-08-18 10:20:09 +0200

    Updated supported payment method parameters.

--- api/paymentmethods/ideal.php
+++ api/paymentmethods/ideal.php
@@ -1,29 +1,15 @@
 <?php
 
-/**
- *  ICEPAY Basicmode API 2
- *  iDeal library
- *
- *  @version 2.5.1
- *  @author Olaf Abbenhuis
- *  @author Jeroen Hehemann
- *  @copyright Copyright (c) 2014, ICEPAY
- *
- */
-
-class Icepay_Paymentmethod_Ideal extends Icepay_Paymentmethod {
-    public      $_version       = "1.0.1";
-    public      $_method        = "IDEAL";
-    public      $_readable_name = "iDEAL";
-    public      $_issuer        = array('ABNAMRO', 'ASNBANK', 'ING', 'KNAB', 'RABOBANK', 'SNSBANK', 'SNSREGIOBANK', 'TRIODOSBANK', 'VANLANSCHOT');
-    public      $_country       = array('NL');
-    public      $_language      = array('NL');
-    public      $_currency      = array('EUR');
-    public      $_amount        = array(
-                                    'minimum'   => 30,
-                                    'maximum'   => 1000000
-                                    );
+class Icepay_Paymentmethod_Ideal extends Icepay_Paymentmethod
+{
+    public $_version       = "2.5.2";
+    public $_method        = "IDEAL";
+    public $_readable_name = "iDEAL";
+    public $_issuer        = array('ABNAMRO', 'ASNBANK', 'ING', 'KNAB', 'RABOBANK', 'SNSBANK', 'SNSREGIOBANK', 'TRIODOSBANK', 'VANLANSCHOT');
+    public $_country       = array('NL');
+    public $_language      = array('NL');
+    public $_currency      = array('EUR');
+    public $_amount        = array('minimum' => 30, 'maximum' => 1000000);
 }
-
 
 ?>
